fc9f1067edea84b6bb58c84d9cab8d839dc264dc
Felipe Marrone <felipemarrone@gmail.com>
2025-06-08 18:47:51 +0100

    First commit

---
+++ src/test/java/com/mydomusfinance/mydomusfinance/MydomusfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mydomusfinance.mydomusfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydomusfinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
